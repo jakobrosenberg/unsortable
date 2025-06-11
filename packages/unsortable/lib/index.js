9ae6b1b1940f4204d54805e722dc5aecdb1e86a6
Jakob Rosenberg <jakobrosenberg@gmail.com>
2025-06-11 11:12:40 +0200

    refactor: update setItems parameter to include target and source context in addDroppable method

--- packages/unsortable/lib/index.js
+++ packages/unsortable/lib/index.js
@@ -188,18 +188,18 @@ export class Unsortable {
    * @param {import('@dnd-kit/dom').DroppableInput['accept']=} [options.accept] - The accepted types for the droppable element.
    * @param {import('@dnd-kit/dom').DroppableInput=} options.droppableOptions - Options for the Droppable instance.
    * @param {T | (() => T) | { set: ((T) => void), get: (() => T)}  } options.items - An accessor for the items associated with the droppable.
-   * @param {(T) => void=} [options.setItems] - A function to set the items in the droppable.
+   * @param {(T, ext:{target: any, source: any}) => void=} [options.setItems] - A function to set the items in the droppable.
    */
   addDroppable(element, options) {
     options.items = toArrayAccessors(options.items)
-    options.items.set = options.setItems || options.items.set
+    options.items['set'] = options.setItems || options.items['set']
 
     const droppable = new Droppable(
       {
-        ...options.droppableOptions,
         id: options.id ?? Math.random().toString(36).substring(2, 15),
         element,
         accept: options.accept,
+        ...options.droppableOptions,
         data: { ...options?.droppableOptions?.data, _unsortable: { ...options, isContainer: true } },
       },
       this.manager,
